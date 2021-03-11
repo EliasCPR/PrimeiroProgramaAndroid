69ba16cacd85266334b223e0cb11d36acdb23626
Elias Mateus Prado Oliveira <eliascpr123@gmail.com>
2021-03-11 10:58:56 -0300

    segunda versão

--- app/src/main/java/br/senai/sp/jandira/calculadoraimc/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/calculadoraimc/MainActivity.kt
@@ -10,18 +10,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        button_limpar.setOnClickListener {
+
+            limparIMC()
+
+        }
+
         button_calcular.setOnClickListener {
 
-            var imc = imc()
-            imc.peso = edit_peso.text.toString().toDouble()
-            imc.altura = edit_altura.text.toString().toDouble()
-
-            text_resultado.text = imc.calcularImc().toString()
-
-            Toast.makeText(this,"Meu primeiro programa Android",Toast.LENGTH_LONG).show()
+            calcularIMC()
 
         }
 
 
+
+
     }
+
+    fun calcularIMC(){
+
+
+        var imc = imc()
+        imc.peso = edit_peso.text.toString().toDouble()
+        imc.altura = edit_altura.text.toString().toDouble()
+
+        text_resultado.text = imc.calcularImc().toString()
+
+        Toast.makeText(this,"Meu primeiro programa Android",Toast.LENGTH_LONG).show()
+    }
+
+    fun limparIMC(){
+
+        var limpar = null
+
+        text_resultado.text = limpar
+        edit_altura.text = limpar
+        edit_peso.text = limpar
+
+    }
+
 }
